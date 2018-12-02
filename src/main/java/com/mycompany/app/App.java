abbf86629012fddd6e28bf49f55c057a2683d8fc
aycagrlyk <gurleyikayca@gmail.com>
2018-12-02 20:29:53 +0300

    com1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,8 +10,63 @@ import java.util.Map;
 
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
+import java.io.File;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
 
 public class App {
+	ArrayList<String> parseList = new ArrayList<>();
+	ArrayList<String> searchList = new ArrayList<>();
+	class UserHandler extends DefaultHandler {
+		   String name="";
+		   String surname="";
+		   String id="";
+		   String res="";
+		   boolean readFirstName = false;
+		   boolean readLastName = false;
+		   boolean readName = false;
+		   
+
+		   @Override
+		   public void startElement(
+		      String uri, String localName, String qName, Attributes attributes)
+		      throws SAXException {
+		      
+		      if (qName.equalsIgnoreCase("NAME")) {
+		         id = attributes.getValue("Id");
+		         res+=id+" ";
+		         System.out.println("Id : " + id);
+		      } else if (qName.equalsIgnoreCase("FIRSTNAME")) {
+		    	  		readFirstName = true;
+		      } else if (qName.equalsIgnoreCase("LASTNAME")) {
+		    	  		readLastName = true;
+		      
+		    
+		   }
+
+		   @Override
+		   public void characters(char ch[], int start, int length) throws SAXException {
+
+		      if (readFirstName) {
+		         res+=name+" ";
+		    	 System.out.println("First Name: " + new String(ch, start, length));
+		         readFirstName = false;
+		      } else if (readLastName) {
+		         res+=surname;
+		         parseList.add(res);
+		         res="";
+		    	 System.out.println("Last Name: " + new String(ch, start, length));
+		         readLastName = false;
+		      }
+		   }
+		}
+		   
+	}
     public static boolean search(ArrayList<Integer> array, int e) {
         System.out.println("inside search");
         if (array == null)
@@ -23,8 +78,11 @@ public class App {
         }
         return false;
     }
+   
 
     public static void main(String[] args) {
+    		      
+    	
         port(getHerokuAssignedPort());
 
         get("/", (req, res) -> "Hello, World");
@@ -32,24 +90,63 @@ public class App {
         post("/compute", (req, res) -> {
             // System.out.println(req.queryParams("input1"));
             // System.out.println(req.queryParams("input2"));
+        	
+        	 try {
+		         File inputFile = new File("EEAS.xml");
+		         SAXParserFactory factory = SAXParserFactory.newInstance();
+		         SAXParser saxParser = factory.newSAXParser();
+		         UserHandler userhandler = new UserHandler();
+		         saxParser.parse(inputFile, userhandler);     
+		      } catch (Exception e) {
+		         e.printStackTrace();
+		      }
+        	
+        	
 
-            String input1 = req.queryParams("input1");
-            java.util.Scanner sc1 = new java.util.Scanner(input1);
-            sc1.useDelimiter("[;\r\n]+");
-            java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
-            while (sc1.hasNext()) {
-                int value = Integer.parseInt(sc1.next().replaceAll("\\s", ""));
-                inputList.add(value);
-            }
-            System.out.println(inputList);
+        	String input1 = req.queryParams("input1").replaceAll("\\s", "");
+            int input1AsInt = Integer.parseInt(input1);
 
             String input2 = req.queryParams("input2").replaceAll("\\s", "");
             int input2AsInt = Integer.parseInt(input2);
+            
+            String person="";
+            String line="";
+            String search="";
+            if(!input1.equals(" ") && input2.equals(" ")) {
+            	for(int i=0;i<parseList.size();i++) {
+            			line=parseList.get(i);
+            			line=line.substring(line.indexOf(" ")+1);
+            			line=line.substring(0,line.indexOf(" "));
+            			
+            		if(input1.equals(line)) {
+            			search+=parseList.get(i)+" ";
+            		}
+            	}
+            }else if(input1.equals(" ") && !input2.equals(" ")) {
+            	for(int i=0;i<parseList.size();i++) {
+        			line=parseList.get(i);
+        			line=line.substring(line.indexOf(" ")+1);
+        			line=line.substring(line.indexOf(" ")+1);
+        			
+        		if(input2.equals(line)) {
+        			search+=parseList.get(i)+" ";
+        		}
+            	}
+            }else if(!input1.equals(" ") && !input2.equals(" ")) {
+            	for(int i=0;i<parseList.size();i++) {
+        			line=parseList.get(i);
+        			line=line.substring(line.indexOf(" ")+1);
+        			
+        		if((input1+" "+input2).equals(line)) {
+        			search+=parseList.get(i)+" ";
+        		 }
+            	}
+            }
 
-            boolean result = App.search(inputList, input2AsInt);
+            
 
             Map map = new HashMap();
-            map.put("result", result);
+            map.put("result", search);
             return new ModelAndView(map, "compute.mustache");
         }, new MustacheTemplateEngine());
 
@@ -68,3 +165,10 @@ public class App {
         return 4567; // return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
+
+
+
+
+
+
+
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -13,49 +13,6 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-
-    public void testFound() {
-        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        assertTrue(new App().search(array, 4));
-    }
-
-    public void testNotFound() {
-        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        assertFalse(new App().search(array, 5));
-    }
-
-    public void testEmptyArray() {
-        ArrayList<Integer> array = new ArrayList<>();
-        assertFalse(new App().search(array, 1));
-    }
-
-    public void testNull() {
-        assertFalse(new App().search(null, 1));
-    }
+    
 
 }
